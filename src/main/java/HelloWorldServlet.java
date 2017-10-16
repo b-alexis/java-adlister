5ab715fbf51c063df8c2c48c89b5f2506003c9c9
becca <r.alexis.tolentino@gmail.com>
2017-10-16 16:25:07 -0500

    if else statement using doGet to display hello + userName else printing hello World

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.soap.AttachmentPart;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+@WebServlet(name = "NameServlet", urlPatterns = "/name")
 
+
+    public class HelloWorldServlet extends HttpServlet {
+        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+            res.setContentType("text/html");
+            String name;
+            name = req.getParameter("name");
+            PrintWriter out = res.getWriter();
+            if (name != null){
+            out.println("<h1> Hello " + name +"</h1>");
+            }else{
+                out.println("<h1> Hello World </h1>");
+            }
+
+
+        }
     }
-}
