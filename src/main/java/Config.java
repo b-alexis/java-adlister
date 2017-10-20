bd0a3d98deab5d1e3e676e059ddf946e3af04c33
becca <r.alexis.tolentino@gmail.com>
2017-10-20 12:32:16 -0500

    MySQLAdsDao created

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,2 +1,24 @@
 public class Config {
+    private String url;
+    private String user;
+    private String password;
+
+    public Config(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public String getPassword(){
+        return password;
+    }
 }
+
